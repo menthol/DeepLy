78e98106d744ed24143e071519a4de7cbdfafdf7
Chris Konnertz <siconize@gmail.com>
2017-12-06 19:41:44 +0100

    Added dummy API key

--- tests/DependenciesTest.php
+++ tests/DependenciesTest.php
@@ -24,7 +24,7 @@ class DependenciesTest extends \PHPUnit\Framework\TestCase
      */
     protected function getInstance()
     {
-        return new ChrisKonnertz\DeepLy\DeepLy();
+        return new ChrisKonnertz\DeepLy\DeepLy('exaxmple-api-key');
     }
 
     public function testGetAndSetSslVerifyPeer()
